4a99fdb3f0352fa9014a257a4e8fd40b44726dec
wangzhen <wangzhenxyz1@msn.com>
2025-09-18 21:44:14 +0800

    feat(events): improve webpack performance

--- web/assembly-worker/build.gradle.kts
+++ web/assembly-worker/build.gradle.kts
@@ -23,9 +23,9 @@ kotlin {
 }
 
 tasks.named("jsBrowserDevelopmentRun") {
-    dependsOn("jsProductionExecutableCompileSync")
+    dependsOn("jsDevelopmentExecutableCompileSync")
 }
 
 tasks.named("jsBrowserProductionWebpack") {
-    dependsOn("jsDevelopmentExecutableCompileSync")
+    dependsOn("jsProductionExecutableCompileSync")
 }
