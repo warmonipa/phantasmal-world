d0a7fdb4b16385cf417c71b7b54eb23b0afc5214
Jason <wangzhenxyz1@msn.com>
2026-05-31 16:40:06 +0800

    Fix item asset parsing byte order

--- web/src/jsMain/kotlin/world/phantasmal/web/viewer/loading/ItemAssetLoader.kt
+++ web/src/jsMain/kotlin/world/phantasmal/web/viewer/loading/ItemAssetLoader.kt
@@ -1,6 +1,7 @@
 package world.phantasmal.web.viewer.loading
 
 import world.phantasmal.core.Success
+import mu.KotlinLogging
 import world.phantasmal.psolib.Endianness
 import world.phantasmal.psolib.buffer.Buffer
 import world.phantasmal.psolib.cursor.cursor
@@ -14,6 +15,8 @@ import world.phantasmal.web.core.loading.AssetLoader
 import world.phantasmal.web.core.loading.LoadingCache
 import world.phantasmal.webui.DisposableContainer
 
+private val logger = KotlinLogging.logger {}
+
 class ItemAssetLoader(private val assetLoader: AssetLoader) : DisposableContainer() {
     private val modelAfsCache: LoadingCache<Unit, List<Buffer>> =
         addDisposable(LoadingCache({ loadAfs("/items/ItemModelEp4.afs") }) {})
@@ -25,25 +28,34 @@ class ItemAssetLoader(private val assetLoader: AssetLoader) : DisposableContaine
         val buffer = modelAfsCache.get(Unit).getOrNull(index)
             ?: throw IllegalArgumentException("Invalid item model index: $index")
 
-        val cursor = buffer.cursor()
+        val cursor = buffer.littleEndianCursor()
         val njResult = parseNj(cursor)
 
-        if (njResult is Success) {
+        if (njResult is Success && njResult.value.isNotEmpty()) {
             return njResult.value.first()
         }
 
-        return parseXj(buffer.cursor()).unwrap().first()
+        return parseXj(buffer.littleEndianCursor()).unwrap().first()
     }
 
     suspend fun loadXvrTextures(index: Int): List<XvrTexture> {
         val buffer = textureAfsCache.get(Unit).getOrNull(index)
             ?: return emptyList()
 
-        return parseXvm(buffer.cursor()).unwrap().textures
+        val result = parseXvm(buffer.littleEndianCursor())
+        return if (result is Success) {
+            result.value.textures
+        } else {
+            logger.warn { "Couldn't parse textures for item model $index." }
+            emptyList()
+        }
     }
 
     private suspend fun loadAfs(path: String): List<Buffer> {
         val buffer = assetLoader.loadArrayBuffer(path)
         return parseAfs(buffer.cursor(Endianness.Little)).unwrap()
     }
+
+    private fun Buffer.littleEndianCursor() =
+        apply { endianness = Endianness.Little }.cursor()
 }
